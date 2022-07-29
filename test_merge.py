55163da0e27de19cf43d0b07bfd21e3588074f43
ciach <ci3l4k@gmail.com>
2022-07-29 06:13:29 +0200

    tests names changed to include information regarding type of test

--- test_merge.py
+++ test_merge.py
@@ -4,7 +4,12 @@ from heapq import merge as hm
 
 
 class TestMerge(unittest.TestCase):
-    def test_merge_one(self):
+    def test_merge_integers_one(self):
         actual = merge([1, 5, 9], [2, 5], [1, 6, 10, 11])
         expected = list(hm([1, 5, 9], [2, 5], [1, 6, 10, 11]))
+        self.assertEqual(actual, expected)
+
+    def test_merge_integers_two(self):
+        actual = merge([1, 3, 5, 7], [0, 2, 4, 8], [5, 10, 15, 20], [], [25])
+        expected = list(hm([1, 3, 5, 7], [0, 2, 4, 8], [5, 10, 15, 20], [], [25]))
         self.assertEqual(actual, expected)
