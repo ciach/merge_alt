153f0ead1bd51fbc88d7c5650ce7623eb65d9315
ciach <ci3l4k@gmail.com>
2022-07-29 11:34:10 +0200

    added reverse function and docstring template

--- merge.py
+++ merge.py
@@ -1,6 +1,19 @@
-def merge(*iterables, key=None):
+def merge(*iterables, key=None, reverse=False) -> list:
+    """_summary_
+
+    Args:
+        key (_type_, optional): _description_. Defaults to None.
+        reverse (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
     rtn_list = []
     for item in iterables:
         for i in item:
             rtn_list.append(i)
-    return sorted(rtn_list, key=key)
+    return sorted(rtn_list, key=key, reverse=reverse)
+
+
+if __name__ == "__main__":
+    print(type(merge([1], [2], [3])))
