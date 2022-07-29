f3221e599dbb8d2d4577c30233acd43415286c02
ciach <ci3l4k@gmail.com>
2022-07-29 07:39:59 +0200

    added no overlap test

--- test_merge.py
+++ test_merge.py
@@ -18,3 +18,25 @@ class TestMerge(unittest.TestCase):
         actual = merge(["dog", "horse"], ["cat", "fish", "kangaroo"], key=len)
         expected = list(hm(["dog", "horse"], ["cat", "fish", "kangaroo"], key=len))
         self.assertEqual(actual, expected)
+
+    def test_merge_performance_one(self):
+        # https://github.com/sweeneyde/multimerge
+        interleaved = [
+            # (0,16,32,...), (1,17,33,...), (2,18,34,...), ...
+            list(map(int, range(x, 16_000, 16)))
+            for x in range(16)
+        ]
+        actual = merge(interleaved)
+        expected = list(hm(interleaved))
+        self.assertEqual(actual, expected)
+
+    def test_merge_performance_two(self):
+        # https://github.com/sweeneyde/multimerge
+        no_overlap = [
+            # (0..999), (1_000..1_999), (2_000..2_999), ...
+            list(map(int, range(x, x + 1_000)))
+            for x in range(0, 16_000, 1_000)
+        ]
+        actual = merge(no_overlap)
+        expected = list(hm(no_overlap))
+        self.assertEqual(actual, expected)
