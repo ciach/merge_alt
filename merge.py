be18b47ef549b236dbac73933a526dc639c549dc
ciach <ci3l4k@gmail.com>
2022-07-29 07:39:22 +0200

    removed if __name__ == main

--- merge.py
+++ merge.py
@@ -4,7 +4,3 @@ def merge(*iterables, key=None):
         for i in item:
             rtn_list.append(i)
     return sorted(rtn_list, key=key)
-
-
-if __name__ == "__main__":
-    print(merge([1, 5, 9], [2, 5], [1, 6, 10, 11]))
