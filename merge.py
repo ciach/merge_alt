63d7bea2c04bc26fccb5b7264fb011c02a345eb3
ciach <ci3l4k@gmail.com>
2022-07-29 06:44:34 +0200

    added 'key' variable

--- merge.py
+++ merge.py
@@ -1,12 +1,10 @@
-def merge(*iterables):
+def merge(*iterables, key=None):
     rtn_list = []
     for item in iterables:
         for i in item:
             rtn_list.append(i)
-    return sorted(rtn_list)
+    return sorted(rtn_list, key=key)
 
 
 if __name__ == "__main__":
     print(merge([1, 5, 9], [2, 5], [1, 6, 10, 11]))
-    # print(concat_generators([1, 2, 3], [2, 3, 4], [3, 4, 5]))
-    # 1, 1, 2, 5, 5, 6, 9, 10, 11
